f8ea00c2a0b5409e9ed16163242cbb6d861ee136
Fabio Guerrero <fabgue@ymail.com>
2019-07-25 17:24:23 -0500

    push script message

--- push-all.sh
+++ push-all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+ echo "message is required" 1>&2
+ exit 1
+fi
+
 git status
 git add -A
-git commit -m 'Cambios'
+git commit -m "$1"
 git push -u origin develop
