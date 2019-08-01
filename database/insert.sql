71e128b0b5f5f44c726c48dcc676a7a7e5f06b7c
Fabio Guerrero <fabgue@ymail.com>
2019-08-01 08:43:55 -0500

    database, flyway

--- database/insert.sql
+++ database/insert.sql
@@ -8,26 +8,34 @@ INSERT INTO hotel_categoria (id, nombre) VALUES(1, '3 Estrellas');
 INSERT INTO hotel_categoria (id, nombre) VALUES(2, '4 Estrellas');
 INSERT INTO hotel_categoria (id, nombre) VALUES(2, '5 Estrellas');
 
+INSERT INTO pais (nombre) VALUES(1, 'COLOMBIA');
+INSERT INTO pais (nombre) VALUES(2, 'VENEZUELA');
 
-INSERT INTO region (id, nombre, tipo, id_region_padre) VALUES(1, 'COLOMBIA', 'PAIS', null);
-INSERT INTO region (id, nombre, tipo, id_region_padre) VALUES(2, 'PANAMA', 'PAIS', null);
-INSERT INTO region (id, nombre, tipo, id_region_padre) VALUES(3, 'ANTIOQUIA', 'DEPARTAMENTO', 1);
-INSERT INTO region (id, nombre, tipo, id_region_padre) VALUES(3, 'BOGOTA', 'DEPARTAMENTO', 1);
-INSERT INTO region (id, nombre, tipo, id_region_padre) VALUES(3, 'ANTIOQUIA', 'DEPARTAMENTO', 1);
-INSERT INTO region (id, nombre, tipo, id_region_padre) VALUES(3, 'BOGOTA', 'DEPARTAMENTO', 1);
+INSERT INTO ciudad (nombre, id_pais) VALUES(1, 'BOGOTA', 1);
+INSERT INTO ciudad (nombre, id_pais) VALUES(2, 'MEDELLIN', 1);
+INSERT INTO ciudad (nombre, id_pais) VALUES(3, 'CARTAGENA', 1);
+INSERT INTO ciudad (nombre, id_pais) VALUES(4, 'CARACAS', 2);
+INSERT INTO ciudad (nombre, id_pais) VALUES(5, 'MARACAIBO', 2);
 
+INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_ciudad, id_hotel_categoria) VALUES('Bacata', 'Cra 1', '300', null, 1, 3);
+INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_ciudad, id_hotel_categoria) VALUES('Bacata', 'Cra 1', '300', null, 1, 3);
+INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_ciudad, id_hotel_categoria) VALUES('Bacata', 'Cra 1', '300', null, 1, 3);
+INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_ciudad, id_hotel_categoria) VALUES('Bacata', 'Cra 1', '300', null, 1, 3);
+INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_ciudad, id_hotel_categoria) VALUES('Bacata', 'Cra 1', '300', null, 1, 3);
+INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_ciudad, id_hotel_categoria) VALUES('Bacata', 'Cra 1', '300', null, 1, 3);
+INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_ciudad, id_hotel_categoria) VALUES('Bacata', 'Cra 1', '300', null, 1, 3);
 
 INSERT INTO habitacion(numero, piso, id_hotel, id_habitacion_tipo) VALUES('', 0, 0, 0);
 
 
 
 
-INSERT INTO hotel (nombre, direccion, telefono, descripcion, id_localidad, id_hotel_categoria) VALUES('', '', '', '', 0, 0);
 
 
 
 
-INSERT INTO localidad (id_region_ciudad, id_region_depto, id_region_pais) VALUES(0, 0, 0);
+
+
 
 
 
